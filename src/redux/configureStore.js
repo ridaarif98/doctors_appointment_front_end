b407360b8b893bb5403dac0d89aa00022c37c8d6
levy002 <levy.ukwi002@gmail.com>
2022-08-16 01:48:55 +0200

    Configure redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,20 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import loginSessionsReducer from "./user/login";
+import registerSessionsReducer from "./user/register";
+
+const rootReducer = combineReducers({
+  loginSessionsReducer,
+  registerSessionsReducer
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: false
+  })
+  ]
+});
+
+export default store;
