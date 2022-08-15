b4b947f09dd4f228c6efd7a8e1982195ab45fbaf
levy002 <levy.ukwi002@gmail.com>
2022-08-16 01:50:12 +0200

    Create login form component and wth success and errors messages

---
+++ src/components/login/Login.js
@@ -0,0 +1,82 @@
+import React,{ useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { userLogin } from '../../redux/user/login';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const [success, setSuccess] = useState('')
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const newState = useSelector((state) => state.loginSessionsReducer)
+
+  const handleChange = (e) => {
+    const input = e.target;
+    const newvalue = input.value;
+    switch (input.name) {
+      case "email":
+        setEmail(newvalue);
+        return email;
+
+      case "password":
+        setPassword(newvalue);
+        return password;
+
+      default:
+        return "yes";
+    }
+  };
+
+  const handleSubmit = (e) => {
+    const form = e.target;
+    e.preventDefault();
+    form.reset();
+    
+    dispatch(userLogin(email, password));
+    
+    if(newState.status === 200) {
+      setSuccess(newState.fetchedData.message)
+      navigate('/home')
+      
+    } else {
+      setErrors(newState.fetchedData.error)
+    }
+  }
+
+  return (
+    <div>
+    <Link to="/">Home</Link>
+    <h2>Login</h2>
+    <p>{errors}</p>
+    <p>{success}</p>
+    <form onSubmit={handleSubmit}> 
+        <input
+          type='email'
+          name='email'
+          value={email}
+          placeholder='Enter your email'
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='text'
+          name='password'
+          placeholder='Enter your password'
+          value={password}
+          onChange={handleChange}
+          required
+          />
+        <input
+          type='submit'
+          value='Login'
+        />
+    </form>
+    </div>
+  )
+}
+
+export default Login;
