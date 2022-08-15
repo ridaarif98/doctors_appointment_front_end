05915d0b8c636a1bf5a24cacce7975521651c088
ridaarif98 <ridaarif16@gmail.com>
2022-08-15 22:45:40 +0200

    Configure route for main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import store from './redux/configureStore';
 import { ViewProvider, MyComponent } from './navigation/ViewPort';
+import AllDoctors from './component/AllDoctors';
+import Doctor from './component/Doctors';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <ViewProvider>
         <MyComponent />
+        <Doctor />
       </ViewProvider>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AllDoctors />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
