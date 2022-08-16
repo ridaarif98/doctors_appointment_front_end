7a0569014ba5d2cc2e4df8f87f3665d5d8ea29a7
ridaarif98 <ridaarif16@gmail.com>
2022-08-16 16:13:45 +0200

    Fix return in doctors

--- src/component/Doctors.js
+++ src/component/Doctors.js
@@ -9,21 +9,19 @@ const Doctor = () => {
     dispatch(getDoctor());
   }, []);
   const doctors = useSelector((state) => state.doctorReducer);
-
+  
   return (
     <div className="all-doctors">
       <h2>Doctors</h2>
-      if (doctors) {
-        <ul className="div list-group">
-          {doctors.map((doctor) => (
-            <li key={doctor.id} className="list-group-item">
-              {doctor.name}
-              <p>{doctor.details}</p>
-              <i>{doctor.image}</i>
-            </li>
-          ))}
-        </ul>
-      }
+      <ul className="div list-group">
+        {doctors.length >0 && doctors.map((doctor) => (
+          <li key={doctor.id} className="list-group-item">
+            {doctor.name}
+            <p>{doctor.details}</p>
+            <i>{doctor.image}</i>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
