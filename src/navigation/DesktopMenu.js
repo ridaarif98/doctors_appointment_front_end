d46bea0c26e9fbef7e2bec88d03ec872376b9664
levy002 <levy.ukwi002@gmail.com>
2022-08-16 18:49:51 +0200

    Render navigation panel conditionally

--- src/navigation/DesktopMenu.js
+++ src/navigation/DesktopMenu.js
@@ -1,9 +1,13 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './navigation.css';
 import image from '../images/consultation.png';
 
 const DesktopMenu = () => {
+  const newState = useSelector((state) => state);
+  const loginStatus = newState.loginSessionsReducer.status;
+  // const registerStatus = newState.registerSessionsReducer.status;
   const Links = [
     {
       id: 1,
@@ -26,43 +30,100 @@ const DesktopMenu = () => {
       text: 'My Appointments',
     },
   ];
+
+  const splashLinks = [
+    {
+      id: 1,
+      path: '/login',
+      text: 'Login',
+    },
+    {
+      id: 2,
+      path: '/register',
+      text: 'Register',
+    },
+  ];
+
   return (
-    <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-      <a className="navbar-brand" href="https://github.com/ridaarif98/doctors_appointment_front_end">
-        <img src={image} width="80" height="80" alt="" />
-      </a>
-      <ul className="nav flex-column">
-        {Links.map((link) => (
-          <li className="nav-item" key={link.id}>
-            {/* <Link className="link-active" to={link.path}> */}
-            <a className="nav-link" href="https://github.com/ridaarif98">{link.text}</a>
-            {/* </Link> */}
-          </li>
-        ))}
-      </ul>
-      <ul className="nav-icons d-flex justify-content-start m-2">
-        <li>
-          <a className="mx-2 p-2" href="https://github.com/ridaarif98">
-            <i className="fa fa-twitter" />
+    <>
+      {
+      loginStatus === '' ? (
+        <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+          <a className="navbar-brand" href="https://github.com/ridaarif98/doctors_appointment_front_end">
+            <img src={image} width="80" height="80" alt="" />
           </a>
-        </li>
-        <li>
-          <a className="mx-2 p-2" href="https://github.com/ridaarif98">
-            <i className="fa fa-facebook-f" />
+          <ul className="nav flex-column">
+            {splashLinks.map((link) => (
+              <li className="nav-item" key={link.id}>
+                <Link className="link-active" to={link.path}>
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="nav-icons d-flex justify-content-start m-2">
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-twitter" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-facebook-f" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-github" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-1" href="https://github.com/ridaarif98">
+                <i className="fa fa-linkedin" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+          <a className="navbar-brand" href="https://github.com/ridaarif98/doctors_appointment_front_end">
+            <img src={image} width="80" height="80" alt="" />
           </a>
-        </li>
-        <li>
-          <a className="mx-2 p-2" href="https://github.com/ridaarif98">
-            <i className="fa fa-github" />
-          </a>
-        </li>
-        <li>
-          <a className="mx-2 p-1" href="https://github.com/ridaarif98">
-            <i className="fa fa-linkedin" />
-          </a>
-        </li>
-      </ul>
-    </nav>
+          <ul className="nav flex-column">
+            {Links.map((link) => (
+              <li className="nav-item" key={link.id}>
+                <Link className="link-active" to={link.path}>
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="nav-icons d-flex justify-content-start m-2">
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-twitter" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-facebook-f" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-github" />
+              </a>
+            </li>
+            <li>
+              <a className="mx-2 p-1" href="https://github.com/ridaarif98">
+                <i className="fa fa-linkedin" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )
+  }
+    </>
   );
 };
 
