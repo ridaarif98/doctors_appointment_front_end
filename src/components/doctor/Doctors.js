29c9df8e8432955a2347f71e03be5f6803d552b3
ridaarif98 <ridaarif16@gmail.com>
2022-08-20 09:00:13 +0200

    Style doctor cards

--- src/components/doctor/Doctors.js
+++ src/components/doctor/Doctors.js
@@ -2,6 +2,7 @@ import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDoctor } from '../../redux/mainpage/mainpage';
 import './doctor.css';
+import image from './img/consultation.png';
 
 const Doctor = () => {
   const dispatch = useDispatch();
@@ -11,16 +12,45 @@ const Doctor = () => {
   const doctors = useSelector((state) => state.doctorReducer);
 
   return (
+
     <div className="all-doctors">
       <h2>Doctors</h2>
-      <ul className="div list-group">
+      <ul className="dctr-list">
         {doctors.length > 0 && doctors.map((doctor) => (
-          <li key={doctor.id} className="list-group-item">
-            {doctor.name}
-            <p>{doctor.details}</p>
-            <i>{doctor.image}</i>
+          <li key={doctor.id} className="card text-center">
+            <img src={image} alt="doctor" className="card-img-top" />
+            <h5 className="card-title mt-5">
+              {' '}
+              {doctor.name}
+            </h5>
+            <p className="card-text text-muted">{doctor.details}</p>
+            <p className="d-flex text-center justify-content-center">
+              <a className="p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-twitter" />
+              </a>
+              <a className="p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-facebook-f" />
+              </a>
+              <a className="p-2" href="https://github.com/ridaarif98">
+                <i className="fa fa-linkedin" />
+              </a>
+            </p>
           </li>
         ))}
+        <li className="card text-center">
+          <img src={image} alt="doctor" />
+          <h5 className="card-title"> TEst</h5>
+
+          <p className="card-text text-muted">bnbnbnbnbnbnbnbnbnbn</p>
+
+        </li>
+        <li className="card text-center">
+          <img src={image} alt="doctor" />
+          <h5 className="card-title"> TEst</h5>
+
+          <p className="card-text text-muted">bnbnbnbnbnbnbnbnbnbn</p>
+
+        </li>
       </ul>
     </div>
   );
