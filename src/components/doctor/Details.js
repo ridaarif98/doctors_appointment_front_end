4ddeb10a20eb3516e44950aacedddc97aa33d471
direakanbi <direakanbi@icloud.com>
2022-08-21 11:08:25 +0100

    Fix Linter Error

--- src/components/doctor/Details.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Details = ({doctorName}) => {
-  return (
-     <h1>{this.prop.doctorName}</h1>
-  );
-};
-
-export default Details;
--- src/components/doctor/DoctorDetails.js
+++ src/components/doctor/DoctorDetails.js
@@ -1,9 +1,8 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getDoctor } from '../../redux/mainpage/mainpage';
 import './doctor.css';
-
 
 const DoctorDetails = () => {
   const dispatch = useDispatch();
@@ -13,33 +12,17 @@ const DoctorDetails = () => {
   const doctors = useSelector((state) => state.doctorReducer);
   const { id } = useParams();
 
-  // return (
-  //   <div className="all-doctors">
-  //     <h2>Doctors</h2>
-  //     <ul className="div list-group">
-  //       {doctors.length > 0 && doctors.map((doctor) => (
-  //         <li key={doctor.id} className="list-group-item"> <Details name={doctor.name}/>
-  //             {doctor.name}
-  //           <p>{doctor.details}</p>
-  //           <i>{doctor.image}</i>
-  //         </li>
-  //       ))}
-  //     </ul> 
-  //   </div>
-  // );
-
   return (
     <ul className="doctor-details">
-     { doctors.filter((doctor) => doctor.id == parseInt(id, 10)).map((doc) => (
-       <li key={doc.id} className="list-group-item">
-                   {doc.name}
-                 <p>{doc.details}</p>
-                <i>{doc.image}</i>
-               </li>
-     ) )
-     }
+      { doctors.filter((doctor) => doctor.id === parseInt(id, 10)).map((doc) => (
+        <li key={doc.id} className="list-group-item">
+          {doc.name}
+          <p>{doc.details}</p>
+          <i>{doc.image}</i>
+        </li>
+      ))}
     </ul>
-  )
+  );
 };
 
 export default DoctorDetails;
