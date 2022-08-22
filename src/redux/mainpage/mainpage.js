66b83260427150736cbe8e1e8695039f3af1c6f9
levy002 <levy.ukwi002@gmail.com>
2022-08-22 20:32:23 +0200

    Post and delete docotor

--- src/redux/mainpage/mainpage.js
+++ src/redux/mainpage/mainpage.js
@@ -1,5 +1,48 @@
+import axios from 'axios';
+
 const FETCH_DOCTORS = 'doctors_appointment_front_end/mainpage/FETCH_DOCTORS';
+const CREATE_DOCTOR = 'GET_DOCTOR';
+const DELETE_DOCTOR = 'DELETE_DOCTOR';
 const initialState = [];
+
+const createDoctor = async (name, fee, details, image) => {
+  const data = await axios({
+    method: 'POST',
+    url: 'http://localhost:3001/api/v1/doctors',
+    data: {
+      name,
+      fee,
+      details,
+      image,
+    },
+  });
+  return data;
+};
+
+export const deleteDoctor = async (doctor) => {
+  const data = await axios.delete(`${'http://localhost:3001/api/v1/doctors'}/${doctor}`);
+  return data;
+};
+
+const createDoctorAction = (data) => ({
+  type: CREATE_DOCTOR,
+  payload: data,
+});
+
+export const createNewDoctorAction = (name, fee, details, image) => async (dispatch) => {
+  const data = await createDoctor(name, fee, details, image);
+  dispatch(createDoctorAction(data.data));
+};
+
+export const deleteDoctorAction = (doctor) => async (dispatch) => {
+  await deleteDoctor(doctor)
+    .then((res) => {
+      dispatch({
+        type: DELETE_DOCTOR,
+        payload: res.data.data,
+      });
+    });
+};
 
 export const getDoctor = () => async (dispatch) => {
   const doctorGet = await fetch('http://[::1]:3001//api/v1/doctors');
@@ -22,6 +65,13 @@ const doctorReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DOCTORS:
       return [...action.payload];
+
+    case CREATE_DOCTOR:
+      return [...action.payload];
+
+    case DELETE_DOCTOR:
+      return state.filter((doc) => doc.id !== action.payload);
+
     default:
       return state;
   }
