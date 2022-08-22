f79deb91bf0d8e302561ec87b4eb17cbd62ab132
levy_ukwishaka <levy.ukwi002@gmail.com>
2022-08-22 21:40:20 +0200

    Fix linter errors

--- src/components/doctor/Doctors.js
+++ src/components/doctor/Doctors.js
@@ -1,7 +1,7 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getDoctor, deleteDoctorAction } from '../../redux/mainpage/mainpage';
 import { Link } from 'react-router-dom';
+import { getDoctor, deleteDoctorAction } from '../../redux/mainpage/mainpage';
 import './doctor.css';
 
 const Doctor = () => {
