3bc9b968acaf33fe7fba724874bc5a4362ea8317
direakanbi <direakanbi@icloud.com>
2022-08-23 21:31:57 +0100

    Updated Design page

--- src/components/doctor/DoctorDetails.js
+++ src/components/doctor/DoctorDetails.js
@@ -1,8 +1,9 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { BsFillGearFill, BsArrowRightCircle, BsFillCaretLeftFill } from 'react-icons/bs';
+import { Link, useParams } from 'react-router-dom';
+import './DoctorDetails.css';
 import { getDoctor } from '../../redux/mainpage/mainpage';
-import './doctor.css';
 
 const DoctorDetails = () => {
   const dispatch = useDispatch();
@@ -14,19 +15,19 @@ const DoctorDetails = () => {
 
   return (
     <ul className="doctor-details">
-      { doctors.filter((doctor) => doctor.id === parseInt(id, 10)).map((doc) => (
+      {doctors.filter((doctor) => doctor.id === parseInt(id, 10)).map((doc) => (
         <li
           className="container mt-5 align-self-center"
           key={doc.id}
           style={
-        {
-          margin: '0 auto',
-        }
-      }
+            {
+              margin: '0 auto',
+            }
+          }
         >
           <div className="row details-pg">
-            <div className="col-md-6 col-12">
-              <img height="450px" width="450px" src={doc.photo} alt="Portrait of doctor Bruno Rodrigues" />
+            <div className="col-md-6 col-12 image">
+              <img height="450px" width="450px" src={doc.image} alt="Portrait of doctor Bruno Rodrigues" />
             </div>
           </div>
 
@@ -39,6 +40,43 @@ const DoctorDetails = () => {
               </p>
             </div>
           </div>
+
+          <table className="table table-striped mt-5">
+            <tbody>
+              <tr>
+                <td>City</td>
+                <td>United States Of America</td>
+              </tr>
+              <tr>
+                <td>Specialization</td>
+                <td>Psychologist</td>
+              </tr>
+              <tr>
+                <td>Cost / Day</td>
+                <td>
+                  $
+                  40
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <Link to="/book_appointment" state={doc.id}>
+            <div className="d-flex justify-content-end reserve-div">
+              <div className="reserve p-2 d-flex">
+                <BsFillGearFill className="mx-2" size={40} color="white" />
+                <button className="btn btn-light reserve-btn" type="submit">Reserve</button>
+                <BsArrowRightCircle className="mx-2" size={40} color="white" />
+              </div>
+            </div>
+          </Link>
+
+          <Link to="/">
+            <div className="main-page-handle-left d-flex justify-content-center align-items-center">
+              <BsFillCaretLeftFill />
+            </div>
+          </Link>
+
         </li>
       ))}
     </ul>
